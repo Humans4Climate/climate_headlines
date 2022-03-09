ae312438fbecd8c422e39454c1fd48bdf998dbba
Humans4Climate <humans@climateheadlines.co>
2022-03-08 20:27:50 -0800

    Add climate_headlines function scripts

---
+++ Climate_Headlines_Update/__init__.py
@@ -0,0 +1,78 @@
+from Climate_Headlines_Update.scripts import database_and_output
+import logging
+import sys
+import os
+from datetime import datetime, timedelta
+from collections import Counter
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+from utils import config
+from scripts import scrapers
+from scripts import database_and_output as db_and_out
+
+import azure.functions as func
+
+class ClimateNewsRunner():
+
+    def __init__(self):
+        # Logging
+        self.logger = logging.getLogger('daily_news_updater')
+        # self.logger.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('(%(asctime)s)%(module)s.%(funcName)s >> %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
+
+        self.date_today = datetime.today().strftime(config.date_format)
+        self.date_yesterday = (datetime.today() - timedelta(days=1)).strftime(config.date_format)
+
+        # Initiate the feedparser class
+        self.parse_rss = scrapers.FeedReader()
+        # Initiate the bing querier class
+        self.bing_news = scrapers.BingNewsScraper(self.date_yesterday)
+        # Initiate the MongoDB update class
+        self.update_mongo = database_and_output.UpdateDB(self.date_today, self.date_yesterday)
+        # Initate gspread class
+        self.write_spreadsheet = database_and_output.UpdateSpreadsheet(self.date_today)
+
+    def run_strategies(self):
+        '''Strategy 1: Get all new news by climate-specific sources previously
+        identified, through RSS queries. No keyword-title filter applied
+            Strategy 2 and 3: Get all news from Bing, set different variables, and 
+        news from RSS with lower priority.
+        Returns None
+        '''
+        strategy_results = {}
+        # Strategy 1
+        # (this and feeds in strategy two follow the same process now)
+        for feed_data in config.rss_climate_specific:
+            parsed_feed_news = self.parse_rss.get_news_one_feed(feed_data, config.strategies[0], keyword_title=True)
+            strategy_results.update(parsed_feed_news)
+        # Strategy 2
+        for feed_data in config.rss_general:
+            parsed_feed_news = self.parse_rss.get_news_one_feed(feed_data, config.strategies[1], keyword_title=True)
+            strategy_results.update(parsed_feed_news)
+        # Strategy 3
+        for keyword in config.use_this_word_for_search:
+            returned_news_search = self.bing_news.get_news_one_keyword(keyword)
+            strategy_results.update(returned_news_search)
+        # Save retrieved news to MongoDB and return counts 
+        documents_updated = self.update_mongo.update_some_data(strategy_results)
+        self.logger.info('All Strategies Mongo Results: || {}'.format(documents_updated))
+        return
+
+    def main(self):
+        self.logger.info('Started Working on Climate News...')
+        # Get news and update MongoDB
+        # self.run_strategies()
+        # write to spreadsheet, today and yesterday's news
+        # 2 sheets: only 50 top news, and all found news
+        # self.write_spreadsheet.main()
+        # Tweet latest piece of news
+        db_and_out.TweetFromDB(self.date_today).tweet_top_new()
+    
+####
+def main(mytimer: func.TimerRequest) -> None:
+
+    ClimateNewsRunner().main()
---
+++ Climate_Headlines_Update/utils/config.py
@@ -0,0 +1,260 @@
+import os
+
+
+# Build secrets
+# MongoDB
+uri = "mongodb+srv://{}:{}@freeclimatecluster.8eg0f.mongodb.net/myFirstDatabase?w=majority&ssl=true&ssl_cert_reqs=CERT_NONE&retrywrites=false".format(os.getenv("mongoazureusr"), os.getenv("mongoazurepwd"))
+# Bing
+bing_headers = {"Ocp-Apim-Subscription-Key":os.getenv("bingstudent")}
+# Google sheets
+gsheet = {
+  "type": "service_account",
+  "project_id": "climatemoneybot",
+  "private_key_id": "5eccb212c944da35bfd77f3778b99706327acc99",
+  "private_key": os.getenv('gsheetpk'),
+  "client_email": "climatemoneybot@climatemoneybot.iam.gserviceaccount.com",
+  "client_id": "115112158692493017800",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/climatemoneybot%40climatemoneybot.iam.gserviceaccount.com"
+}
+
+t_api_keys = (os.getenv("taccesskey"), os.getenv("taccesskeysecret"))
+t_api_tokens = (os.getenv("taccesstoken"), os.getenv("taccesstokensecret"))
+
+# Null document on spreadsheet/airtable
+null_doc = {
+    'null':{
+        'title':'null',
+        'summary':'null',
+        'date':'null',
+        'date_time':'null',
+        'api':'null',
+        'tweeted':'null',
+        'news_source':'null',
+        'keywords':'null',
+        'query':'null',
+        'thumbnail':'null',
+        'source_thumbnail':'null',
+        'strategy':'null'
+        }
+    }
+
+# Default images if none found
+default_thumbnail = 'https://images.unsplash.com/photo-1570358934836-6802981e481e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80'
+default_source_thumbnail = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/google/298/rolled-up-newspaper_1f5de-fe0f.png'
+
+
+
+limit_news_sent = 50
+strategies = ['major_climate_outlets','other_major_outlets','remaining_outlets']
+
+# Spreadsheet config
+document_length = 13
+row_length = 10
+row_letters = 'A:J'
+letters = ['A','B','C','D','E','F','G','H','I','J']
+cols = ['URL', 'Title', 'Summary', 'Source', 'Date-Time', 'Keywords', 'Thumbnail', 'Source Thumbnail', 'Strategy', 'Rank']
+col_widths = [
+    250, # URL
+    400, # Title
+    500,  # Summary
+    150, # Source
+    120, # date-time
+    100,  # keywords
+    100, # thumbnail
+    100, # provider thumbnail,
+    100,  # strategy (for bookkeeping, not sent to the.com)
+    20 # ranking (for bookkeeping, not sent to the.com)
+    ]
+
+    
+# Date_times
+time_format = '%Y-%m-%d %H:%M'
+date_format = '%Y-%m-%d'
+date_time_format = '%I:%M%p (%b %d %Y)'
+
+### Aggregating Strategies
+
+# Strategy 1: Major Climate Publications
+# Show all news from major climate-specific sources (no keyword search)
+rss_climate_specific = [
+    ('Carbon Brief','https://feeds.feedburner.com/carbonbrief'),
+    ('NASA Climate','https://climate.nasa.gov/news/rss.xml'),
+    ('Reuters Environment','https://www.reutersagency.com/feed/?best-topics=environment&post_type=best'),
+    ('New York Times Climate','https://rss.nytimes.com/services/xml/rss/nyt/Climate.xml'),
+    ('New York Times Energy','https://rss.nytimes.com/services/xml/rss/nyt/EnergyEnvironment.xml'),
+    ('Politico Energy & Environment','https://rss.politico.com/energy.xml'),
+    ('The Guardian Climate Change','https://www.theguardian.com/environment/climate-change/rss'),
+    ('The Guardian Energy','https://www.theguardian.com/environment/energy/rss'),
+    ('The Guardian Environment','https://www.theguardian.com/environment/rss'),
+    ('Nature Climate','http://feeds.nature.com/nclimate/rss/current'),
+    ('TechCrunch Green','http://feeds.feedburner.com/TechCrunch/greentech'),
+    ('BBC Climate and Environment','http://feeds.bbci.co.uk/news/science_and_environment/rss.xml'),
+    ('UN Climate','https://news.un.org/feed/subscribe/en/news/topic/climate-change/feed/rss.xml'),
+    ('US Energy Information Administration','https://www.eia.gov/rss/todayinenergy.xml'),
+    ('EPA Climate Change','https://www.epa.gov/newsreleases/search/rss/subject/climate'),
+    ('EPA Energy','https://www.epa.gov/newsreleases/search/rss/subject/energy'),
+    ('EPA International','https://www.epa.gov/newsreleases/search/rss/subject/international'),
+    ('The Global Warming Policy Foundation','https://www.thegwpf.org/feed/'),
+    ('Canary Media','https://www.canarymedia.com/rss'),
+    ('Job One for Humanity','https://www.joboneforhumanity.org/blog.rss'),
+    ('Shell Climate Change','https://blogs.shell.com/feed/'),
+    # ('Climate Change Dispatch','https://follow.it/climate-change-dispatch'), # Anti-climate
+    ('Columbia Law Climate','http://blogs.law.columbia.edu/climatechange/feed/'),
+    ('Union of Concerned Scientists','https://blog.ucsusa.org/category/global-warming/feed'),
+    ('Environmental Defense Fund','http://blogs.edf.org/edfish/feed/')
+]
+
+# Strategy 2: Other Trusted Publications
+# News from major sources, but source is not specifically climate - keyword search
+# Both from feeds and from Bing News, but filter trusted Bing sources
+rss_general = [    
+    ('CNN Technology','http://rss.cnn.com/rss/cnn_tech.rss'),
+    ('CNN Business','http://rss.cnn.com/rss/money_latest.rss'),
+    ('CNN World','http://rss.cnn.com/rss/cnn_world.rss'),
+    ('CNN Top Stories','http://rss.cnn.com/rss/cnn_topstories.rss'),
+    ('Reuters Busines Finance','https://www.reutersagency.com/feed/?best-topics=business-finance&post_type=best'),
+    ('New York Times World','https://rss.nytimes.com/services/xml/rss/nyt/World.xml'),
+    ('New York Times Business','https://rss.nytimes.com/services/xml/rss/nyt/Business.xml'),
+    # {'The Washington Times Business & Economy','https://www.washingtontimes.com/rss/headlines/news/business-economy/'),
+    # {'The Washington Times Technology','https://www.washingtontimes.com/rss/headlines/culture/technology/'),
+    ('Wired Business','https://www.wired.com/feed/category/business/latest/rss'),
+    ('Wired Science','https://www.wired.com/feed/category/science/latest/rss'),
+    ('Nature','http://feeds.nature.com/nature/rss/current'),
+    ('Science Daily','http://feeds.sciencedaily.com/sciencedaily'),
+    ('Utility Dive','https://www.utilitydive.com/feeds/news/'),
+    ('TechCrunch Startups','http://feeds.feedburner.com/TechCrunch/startups'),
+    ('TechCrunch Fundings','http://feeds.feedburner.com/TechCrunch/fundings-exits'),
+    ('Australia Business Telegraph','https://www.dailytelegraph.com.au/business/breaking-news/rss'),
+    ('Wall Street Journal: World','https://feeds.a.dj.com/rss/RSSWorldNews.xml'),
+    ('Wall Street Journal: US Business','https://feeds.a.dj.com/rss/WSJcomUSBusiness.xml'),
+    ('Vegconomist','https://vegconomist.com/feed/'),
+    ('Utility Dive','https://www.utilitydive.com/feeds/news/'),
+    ('The Conversation','https://theconversation.com/us/environment/articles.atom'),
+    # {'Wattsupwiththat','https://wattsupwiththat.com/feed/'),
+    ('The Telegraph','https://www.telegraph.co.uk/news/rss.xml'),
+    ('Skeptical Science','https://skepticalscience.com/feed.xml'),
+    ('Commondreams','https://www.commondreams.org/rss.xml'),
+    ('Greenpeace Australia','https://www.greenpeace.org.au/feed/')
+    #{'International Energy Association','https://www.iea.org/news-and-events'}
+]
+trusted_bing_sources = [
+    'Axios',
+    'YAHOO!News',
+    'The Hill',
+    'The Hill on MSN.com',
+    'San Francisco Chronicle',
+    'Business Insider',
+    'CNBC',
+    'The Telegraph on MSN.com',
+    'Washington Examiner',
+    'Quartz',
+    'Calgary Herald',
+    'The Daily Telegraph',
+    'Quartz on MSN.com',
+    'South China Morning Post on MSN.com',
+    'New York Post',
+    'Electrek',
+    'Business Weekly',
+    'Reuters'
+]
+
+# Strategy 3: Climate All Around the web
+# "everything else", i.e. Bing News results not from trusted sources
+
+# Keywords
+save_if_this_word_in_title = [
+    # narrower list
+    'climate', 
+    'carbon', 
+    'offset', 
+    'hydrogen', 
+    'sustainab',
+    'dioxide', 
+    'batteries', 
+    'electrification', 
+    # 'environment', 
+    #'emission', 
+    'greenhouse', 
+    'methane', 
+    'acidification', 
+    'solar', 
+    'electric vehicle', 
+    #'energy',
+    'offshore', 
+    #'electric', 
+    #'rise', 
+    'sea level', 
+    'coal', 
+    'natural gas', 
+    #'nuclear', 
+    'glacier', 
+    'activist',
+    #'plastic', 
+    'fracking', 
+    'e.p.a.', 
+    #'earth', 
+    #'planet', 
+    'environmental', 
+    'cop26', 
+    # 'endangered', 
+    'renewable',
+    # 'greta', 
+    # 'pipeline', 
+    'wildfire',
+    # 'heating', 
+    # 'warming',
+    # 'nature'
+    #'epa', 'esg', 'green', 'gsg', 'fuel'
+]
+
+use_this_word_for_search = [
+    # broader list
+    'climate', 
+    'climate change',
+    'global warming',
+    'carbon', 
+    'co2',
+    'climate tech',
+    'renewable',
+    'offset', 
+    'hydrogen', 
+    'sustainable', 
+    'dioxide', 
+    'batteries', 
+    'electrification', 
+    'environment', 
+    'emission', 
+    'greenhouse', 
+    'methane', 
+    'acidification', 
+    'solar', 
+    'electric vehicle', 
+    'energy',
+    'offshore', 
+    #'electric', 
+    #'rise', 
+    'sea level', 
+    'coal', 
+    'natural gas', 
+    'nuclear', 
+    'glacier', 
+    'activist',
+    #'plastic', 
+    'fracking', 
+    'e.p.a.', 
+    #'earth', 
+    #'planet', 
+    'sutainability',
+    'environmental', 
+    'cop26', 
+    'endangered', 
+    # 'greta', 
+    # 'pipeline', 
+    'wildfire',
+    'heating',
+    # 'nature'
+    #'epa', 'esg', 'green', 'gsg', 'fuel'
+]
